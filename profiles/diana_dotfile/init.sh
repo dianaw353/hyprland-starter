0dbc8d06bca83154dfedd8e6dd9023391e029db1
Diana <65094216+dianaw353@users.noreply.github.com>
2024-03-24 20:31:23 -0600

    Update init.sh

--- profiles/diana_dotfile/init.sh
+++ profiles/diana_dotfile/init.sh
@@ -1,25 +1,35 @@
-figlet "Diana Starter Dotfiles"
-source ./library.sh
+#!/bin/bash
+
+# Diana Starter Dotfiles
+source ./../../library.sh
+
 read -p "Press enter to continue install the dotfiles."
+
 echo "Downloading dotfiles"
 cd ~/
 git clone --depth=1 https://github.com/dianaw353/starter-dotfile.git
 cd ~/starter-dotfile
+
 echo "Installing dotfiles dependencies"
 _installPackageAur "profiles/diana_dotfiles/packages.txt"
+
 echo "Installing dotfiles"
 if [ -d ~/dotfiles-versions/$version/alacritty ]; then
     _installSymLink alacritty ~/.config/alacritty ~/dotfiles/alacritty/ ~/.config
 fi
+
 if [ -d ~/dotfiles-versions/$version/hypr ]; then
     _installSymLink hypr ~/.config/hypr ~/dotfiles/hypr/ ~/.config
 fi
+
 if [ -d ~/dotfiles-versions/$version/wlogout ]; then
     _installSymLink wlogout ~/.config/wlogout ~/dotfiles/wlogout/ ~/.config
 fi
+
 if [ -d ~/dotfiles-versions/$version/dunst ]; then
     _installSymLink dunst ~/.config/dunst ~/dotfiles/dunst/ ~/.config
 fi
+
 cd ~/hyprland-starter
 # echo "Downloading wallpaper"
 # echo "Installing wallpaper"
