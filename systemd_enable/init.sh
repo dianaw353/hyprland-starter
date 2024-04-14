272c55e539ef614a4cc67dca8b29275c5f284925
Diana <diana.ward535@gmail.com>
2024-04-14 17:21:46 -0600

    update

--- systemd_enable/init.sh
+++ systemd_enable/init.sh
@@ -18,7 +18,7 @@ if gum confirm "Do you want to autostart hyprland upon system boot?"; then
   echo "Installing greetd..."
   sudo pacman -S --needed --noconfirm greetd
   current_user=$(whoami)
-  sed -i "s|^user = \"diana\"|user = \"$current_user\"|" config.toml
+  sed -i "s|^user = \"diana\"|user = \"$current_user\"|" systemd_enable/config.toml
   sudo mv ~/hyprland-starter/systemd_enable/config.toml /etc/greetd
 else
   echo "NOT autostarting hyprland upon system boot"
