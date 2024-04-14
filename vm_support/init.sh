0e899eed1501e459b390844a9a5cb64e62baefe8
Diana <diana.ward535@gmail.com>
2024-04-14 17:18:41 -0600

    update

--- vm_support/init.sh
+++ vm_support/init.sh
@@ -15,8 +15,10 @@ if [ $(_isKVM) == "0" ] ;then
         echo ":: KVM Environment already set."
     else
         if gum confirm "Do you want to install the KVM environment variables?" ;then
-            echo "source = ~/dotfiles/hypr/conf/environments/kvm.conf" >  ~/dotfiles-versions/$version/hypr/conf/environment.conf
-            echo "Environment set to KVM."
+            echo "Setting environment variables for NVIDIA drivers..."
+        cat > ~/dotfiles/hypr/conf/environment.conf << EOF
+source = ~/dotfiles/hypr/conf/environments/kvm.conf
+EOF
         fi
     fi
     if [[ $(_installPackagesPacman "${pkg}") == 0 ]]; then
@@ -28,17 +30,4 @@ if [ $(_isKVM) == "0" ] ;then
     fi
 fi
 
-updateConfigFile() {
-    # Ask the user if they are using a VM using Gum
-    if gum confirm "Are you using a VM?"; then
-        # Add a line to the config file
-        echo "env = WLR_NO_HARDWARE_CURSORS, 1" >> "$HOME/.config/hypr/conf/env/default.conf"
-        echo "env = WLR_RENDERER_ALLOW_SOFTWARE, 1" >> "$HOME/.config/hypr/conf/env/default.conf"
-        echo "Configuration updated for VM usage."
-    else
-        echo "No changes made to the configuration."
-    fi
-}
 
-# Call the function to update the config file
-#updateConfigFile
