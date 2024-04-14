fbbe8d1f3df8d118179dfc8648f38af011655197
Diana <diana.ward535@gmail.com>
2024-04-14 13:48:54 -0600

    update

--- gpu_drivers/init.sh
+++ gpu_drivers/init.sh
@@ -35,7 +35,19 @@ installWine() {
     question="Do you want to install wine? It's a compatibility layer for running Windows applications on Linux."
     if gum confirm "$question"; then
         echo "Installing wine..."
-        sudo pacman -S wine
+        options=("amd" "intel" "nvidia")
+        selected_brands=$(gum choose --no-limit "${options[@]}")
+        for selected_brand in $selected_brands; do
+            local file_path="32bit${selected_brand}.txt"
+            echo "Resolved file path: $(readlink -f "$file_path")"
+
+            if [ -f "$file_path" ]; then
+                echo "Installing 32bit GPU drivers for $selected_brand from file: $file_path"
+                _installPackagesPacman "$file_path" || echo "ERROR: Failed to install 32bit GPU drivers for $selected_brand"
+            else
+                echo "No 32bit GPU drivers found for $selected_brand"
+            fi
+        done
     else
         echo "Skipping wine installation."
     fi
