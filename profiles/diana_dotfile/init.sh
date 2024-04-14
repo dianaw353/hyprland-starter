ecdbfad27ec02c9c91ed3edfe7055f86ed74e82d
Diana <diana.ward535@gmail.com>
2024-04-14 13:05:57 -0600

    update

--- profiles/diana_dotfile/init.sh
+++ profiles/diana_dotfile/init.sh
@@ -4,39 +4,26 @@ read -p "Press enter to continue install the dotfiles."
 echo "Downloading dotfiles"
 cd ~/
 git clone --depth=1 https://github.com/dianaw353/starter-dotfile.git
-cd ~/hyprland-starter
-echo "Installing dotfiles dependencies"
-_installPackageAur "/home/$(whoami)/hyprland-starter/profiles/diana_dotfile/packages.txt"
 cd ~/starter-dotfile
+echo "Installing dotfiles dependencies"
+_installPackageAur "/home/$(whoami)/starter-dotfile/profiles/diana_dotfile/packages.txt"
 version=$(cat .version/version)
 echo "$version"
 echo "Copping dotfiles to $version"
 mkdir ~/dotfiles-versions
 mkdir ~/dotfiles-versions/$version
 cp -r ~/starter-dotfile/alacritty ~/dotfiles-versions/$version/
-#cp -r ~/starter-dotfile/dunst ~/dotfiles-versions/$version/
 cp -r ~/starter-dotfile/gtk ~/dotfiles-versions/$version/
 cp -r ~/starter-dotfile/hypr ~/dotfiles-versions/$version/
-#cp -r ~/starter-dotfile/wlogout ~/dotfiles-versions/$version/
 mkdir ~/dotfiles
 echo "Installing dotfiles"
 if [ -d ~/dotfiles-versions/$version/alacritty ]; then
-    _installSymLink alacritty ~/.config/alacritty ~/dotfiles/alacritty ~/.config
+    _installSymLink alacritty ~/.config/alacritty ~/dotfiles-versions/$version/alacritty ~/.config
 fi
 if [ -d ~/dotfiles-versions/$version/hypr ]; then
-    _installSymLink hypr ~/.config/hypr ~/dotfiles/hypr ~/.config
+    _installSymLink hypr ~/.config/hypr ~/dotfiles-versions/$version/hypr ~/.config
 fi
 if [ -d ~/dotfiles-versions/$version/gtk ]; then
-    _installSymLink gtk ~/.config/gtk ~/dotfiles/gtk ~/.config
+    _installSymLink gtk ~/.config/gtk ~/dotfiles-versions/$version/gtk ~/.config
 fi
-#if [ -d ~/dotfiles-versions/$version/wlogout ]; then
-#    _installSymLink wlogout ~/.config/wlogout ~/dotfiles/wlogout ~/.config
-#fi
-#if [ -d ~/dotfiles-versions/$version/dunst ]; then
-#    _installSymLink dunst ~/.config/dunst ~/dotfiles/dunst ~/.config
-#fi
 cd ~/hyprland-starter
-# echo "Downloading wallpaper"
-# echo "Installing wallpaper"
-# echo "Setting up greetd"
-# echo "Switching shell to zsh"
