5d3d5254c1f463e92281091a865c8e90d4645d56
Diana <diana.ward535@gmail.com>
2024-04-14 15:39:01 -0600

    update

--- gpu_drivers/init.sh
+++ gpu_drivers/init.sh
@@ -1,73 +1,64 @@
 #!/bin/bash
+
 figlet "GPU Drivers"
-source ./library.sh
+
+source "./library.sh"
+
 echo "What GPU drivers would you like to install?"
-## Function to select GPU drivers for multiple brands
-selectGPUDrivers() {
-    options=("amd" "intel" "nvidia")
-    selected_brands=$(gum choose --no-limit "${options[@]}")
-    echo "$selected_brands"
+
+select_gpu_drivers() {
+  options=("amd" "intel" "nvidia")
+  gum choose --no-limit "${options[@]}"
 }
 
-_installPackagesPacman() {
-  local packages=$1
-  echo "Installing packages: $packages"
-  sudo pacman -S --needed --noconfirm "$packages"
-}
-
-installGPUDrivers() {
+install_gpu_drivers() {
   local selected_brands=$1
   for selected_brand in $selected_brands; do
     local file_path="gpu_drivers/${selected_brand}.txt"
     echo "Resolved file path: $(readlink -f "$file_path")"
 
-    if [ -f "${selected_brand}.txt" ]; then
-      echo "File ${selected_brand}.txt exists. Installing GPU drivers for $selected_brand..."
-      if _installPackagesPacman "$(cat "$file_path")"; then
-        echo "GPU drivers for $selected_brand installed successfully"
-        if [ "$selected_brand" == "nvidia" ]; then
-          echo "Setting environment variables for NVIDIA drivers..."
-          cat > ~/starter-dotfile/hypr/conf/environment.conf << EOF
+    if [ -f "$file_path" ]; then
+      echo "Installing GPU drivers for $selected_brand from file: $file_path"
+      _install_packages_pacman "$(cat "$file_path")" || echo "ERROR: Failed to install GPU drivers for $selected_brand"
+      if [ "$selected_brand" == "nvidia" ]; then
+        echo "Setting environment variables for NVIDIA drivers..."
+        cat > ~/starter-dotfile/hypr/conf/environment.conf << EOF
 source = ~/starter-dotfile/hypr/conf/environments/nvidia.conf
 EOF
-        fi
-      else
-        echo "ERROR: Failed to install GPU drivers for $selected_brand"
       fi
     else
-      echo "File ${selected_brand}.txt does not exist. Skipping GPU drivers for $selected_brand..."
+      echo "No GPU drivers found for $selected_brand"
     fi
   done
 }
 
-## Function to install wine if the user chooses to install it
-installWine() {
-    question="Do you want to install wine? It's a compatibility layer for running Windows applications on Linux."
-    if gum confirm "$question"; then
-        echo "Installing wine..."
-        options=("amd" "intel" "nvidia")
-        selected_brands=$(gum choose --no-limit "${options[@]}")
-        for selected_brand in $selected_brands; do
-            local file_path="gpu_drivers/32bit${selected_brand}.txt"
-            echo "Resolved file path: $(readlink -f "$file_path")"
+install_wine() {
+  question="Do you want to install wine? It's a compatibility layer for running Windows applications on Linux."
+  if gum confirm "$question"; then
+    echo "Installing wine..."
+    options=("amd" "intel" "nvidia")
+    selected_brands=$(gum choose --no-limit "${options[@]}")
+    for selected_brand in $selected_brands; do
+      local file_path="gpu_drivers/32bit${selected_brand}.txt"
+      echo "Resolved file path: $(readlink -f "$file_path")"
 
-            if [ -f "32bit${selected_brand}.txt" ]; then
-                echo "File 32bit${selected_brand}.txt exists. Installing 32bit GPU drivers for $selected_brand..."
-                if _installPackagesPacman "$(cat "$file_path")"; then
-                  echo "32bit GPU drivers for $selected_brand installed successfully"
-                else
-                  echo "ERROR: Failed to install 32bit GPU drivers for $selected_brand"
-                fi
-            else
-                echo "File 32bit${selected_brand}.txt does not exist. Skipping 32bit GPU drivers for $selected_brand..."
-            fi
-        done
-    else
-        echo "Skipping wine installation."
-    fi
+      if [ -f "$file_path" ]; then
+        echo "Installing 32bit GPU drivers for $selected_brand from file: $file_path"
+        _install_packages_pacman "$(cat "$file_path")" || echo "ERROR: Failed to install 32bit GPU drivers for $selected_brand"
+      else
+        echo "No 32bit GPU drivers found for $selected_brand"
+      fi
+    done
+  else
+    echo "Skipping wine installation."
+  fi
 }
 
-## Example usage:
-selected_brands=$(selectGPUDrivers)
-installGPUDrivers "$selected_brands"
-installWine
+_install_packages_pacman() {
+  local packages=$1
+  sudo pacman -S --needed --noconfirm "$packages"
+}
+
+selected_brands=$(select_gpu_drivers)
+install_gpu_drivers "$selected_brands"
+install_wine
