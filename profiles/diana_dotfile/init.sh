45fc153d1cd1e5d6b507399cb7be9e16e9e10cf0
Diana <diana.ward535@gmail.com>
2024-04-14 18:44:20 -0600

    update

--- profiles/diana_dotfile/init.sh
+++ profiles/diana_dotfile/init.sh
@@ -17,16 +17,58 @@ cp -r ~/starter-dotfile/gtk ~/dotfiles-versions/$version/
 cp -r ~/starter-dotfile/hypr ~/dotfiles-versions/$version/
 mkdir ~/dotfiles
 cp -r ~/starter-dotfile/alacritty ~/dotfiles/
-cp -r ~/starter-dotfile/gtk ~/dotfiles/
 cp -r ~/starter-dotfile/hypr ~/dotfiles/
+# Installing GTK Files
+# Remove existing symbolic links
+gtk_symlink=0
+gtk_overwrite=1
+if [ -L ~/.config/gtk-3.0 ]; then
+  rm ~/.config/gtk-3.0
+  gtk_symlink=1
+fi
+
+if [ -L ~/.config/gtk-4.0 ]; then
+  rm ~/.config/gtk-4.0
+  gtk_symlink=1
+fi
+
+if [ -L ~/.gtkrc-2.0 ]; then
+  rm ~/.gtkrc-2.0
+  gtk_symlink=1
+fi
+
+if [ -L ~/.Xresources ]; then
+  rm ~/.Xresources
+  gtk_symlink=1
+fi
+
+if [ "$gtk_symlink" == "1" ] ;then
+  echo ":: Existing symbolic links to GTK configurations removed"
+fi
+
+if [ -d ~/.config/gtk-3.0 ] ;then
+  echo "The script has detected an existing GTK configuration."
+  if gum confirm "Do you want to overwrite your configuration?" ;then
+    gtk_overwrite=1
+  else
+    gtk_overwrite=0
+  fi
+fi
+
+if [ "$gtk_overwrite" == "1" ] ;then
+  cp -r gtk/gtk-3.0 ~/.config/
+  cp -r gtk/gtk-4.0 ~/.config/
+  cp -r gtk/xsettingsd ~/.config/
+  cp gtk/.gtkrc-2.0 ~/
+  cp gtk/.Xresources ~/
+  echo ":: GTK Theme installed"
+fi
+
 echo "Installing dotfiles"
 if [ -d ~/dotfiles-versions/$version/alacritty ]; then
     _installSymLink alacritty ~/.config/alacritty ~/dotfiles/alacritty ~/.config
 fi
 if [ -d ~/dotfiles-versions/$version/hypr ]; then
     _installSymLink hypr ~/.config/hypr ~/dotfiles/hypr ~/.config
-fi
-if [ -d ~/dotfiles-versions/$version/gtk ]; then
-    _installSymLink gtk ~/.config/gtk ~/dotfiles/gtk ~/.config
 fi
 cd ~/hyprland-starter
