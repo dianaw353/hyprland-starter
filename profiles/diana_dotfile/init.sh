4d4a5ce0fc2b3cc2c8f61feb025a604a9938c6c3
Diana <diana.ward535@gmail.com>
2024-04-17 20:22:50 -0600

    update

--- profiles/diana_dotfile/init.sh
+++ profiles/diana_dotfile/init.sh
@@ -3,7 +3,7 @@ source ./library.sh
 read -p "Press enter to continue install the dotfiles."
 echo "Downloading dotfiles"
 echo "Installing dotfiles dependencies"
-_installPackageAur "./packages.txt"
+_installPackageAur "profiles/diana_dotfile/packages.txt"
 cd ~/
 git clone --depth=1 https://github.com/dianaw353/starter-dotfile.git
 cd ~/starter-dotfile
