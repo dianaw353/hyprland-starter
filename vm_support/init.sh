0c9ecefd04e659b8d4aa92681827c0ab752c124a
Diana <diana.ward535@gmail.com>
2024-04-19 20:39:29 -0600

    update

--- vm_support/init.sh
+++ vm_support/init.sh
@@ -1,33 +1,21 @@
+#!/bin/bash
+
 _isKVM() {
-    iskvm=$(sudo dmesg | grep "Hypervisor detected")
-    if [[ "$iskvm" =~ "KVM" ]] ;then
-        echo 0
-    else
-        echo 1
-    fi
+  if [[ $(systemd-detect-virt) == "kvm" ]]; then
+    echo "1"
+  else
+    echo "0"
+  fi
 }
 
-if [ $(_isKVM) == "0" ] ;then
-    figlet "KVM VM"
-    echo "The script has detected that you run the installation in a KVM (virtual machine)."
-    if grep -Fxq "kvm.conf" ~/dotfiles/hypr/conf/environment.conf
-    then
-        echo ":: KVM Environment already set."
-    else
-        if gum confirm "Do you want to install the KVM environment variables?" ;then
-            echo "Setting environment variables for KVM..."
-        cat > ~/dotfiles/hypr/conf/environment.conf << EOF
-source = ~/dotfiles/hypr/conf/environments/kvm.conf
-EOF
-        fi
-    fi
-    if [[ $(_installPackagesPacman "${pkg}") == 0 ]]; then
-        echo ":: Qemu Guest Agent already installed"
-    else
-        if gum confirm "Do you want to install the QEMU guest agent?" ;        then
-        sudo pacman -S --needed --noconfirm qemu-guest-agent;
-        fi
-    fi
+if [ $(_isKVM) == "0" ]; then
+  figlet "KVM VM"
+
+  # Ask the user if they want to install the KVM environment variables using gum confirm
+  if gum confirm "Do you want to install KVM environment variables?"
+  then
+    # Change the end of the first line in the environment.conf file to kvm.conf
+    sed -i 's/^.*$/& kvm.conf/' ~/dotfiles/hypr/conf/environment.conf
+    figlet "KVM environment variables installed!"
+  fi
 fi
-
-
